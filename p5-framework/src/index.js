d31ab06c3b3d8b73365265aea317d0580a9f550e
Ludo <ludoloops@protonmail.com>
2020-06-01 19:00:31 +0200

    feat: nav bar

--- p5-framework/src/index.js
+++ p5-framework/src/index.js
@@ -1,13 +1,14 @@
 import './styles/styles.css';
 import './styles/styles.scss';
 import logoPath from './images/logo.svg';
+import hamburgerPath from './images/hamburger.svg';
 
 const logoImg = document.getElementById('logo');
 logoImg.src = logoPath;
 
+const hamburgerImg = document.getElementById('hamburger');
+hamburgerImg.src = hamburgerPath;
 
 const version = 4;
 
 document.getElementById('#title').html(`bonjour de Webpack ${version}`);
-
-
