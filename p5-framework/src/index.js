0e557ba335d9c0ebbb253d05f029dfe950e1f2dc
Ludo <ludoloops@protonmail.com>
2020-06-01 17:37:00 +0200

    feat: css purge pour tailwindCss

--- p5-framework/src/index.js
+++ p5-framework/src/index.js
@@ -1,11 +1,13 @@
 import './styles/styles.css';
 import './styles/styles.scss';
-import homeIcon from './images/logo.svg';
+import logoPath from './images/logo.svg';
 
-const homeImg = document.getElementById('home');
-homeImg.src = homeIcon;
+const logoImg = document.getElementById('logo');
+logoImg.src = logoPath;
 
 
 const version = 4;
 
 document.getElementById('#title').html(`bonjour de Webpack ${version}`);
+
+
--- p5-framework/webpack.prod.js
+++ p5-framework/webpack.prod.js
@@ -1,10 +1,17 @@
 /* eslint-disable */
+const path = require('path');
+const glob = require('glob');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJsPlugin = require('terser-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 /* eslint-enable */
 const common = require('./webpack.common');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+};
 
 module.exports = merge(common, {
   mode: 'production',
@@ -36,6 +43,9 @@ module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/[name]-[contenthash].css',
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
   ],
   optimization: {
