14990db37dcafbbe2682594ce6a44fd0a7edb78c
Ludo <ludoloops@protonmail.com>
2020-06-03 01:23:31 +0200

    Feat ajout hero

--- p5-framework/src/index.js
+++ p5-framework/src/index.js
@@ -2,13 +2,15 @@ import './styles/styles.css';
 import './styles/styles.scss';
 import logoPath from './images/logo.svg';
 import favicon from './images/favicon.png';
+import heroPath from './images/camera.jpg';
 import urlServer from './modules/secret/urlServer';
 
 const logoImg = document.getElementById('logo');
 logoImg.src = logoPath;
-
 const faviconImg = document.getElementById('favicon');
 faviconImg.href = favicon;
+const heroImg = document.getElementById('hero');
+heroImg.style.backgroundImage = `url(${heroPath})`;
 
 const version = 4;
 
