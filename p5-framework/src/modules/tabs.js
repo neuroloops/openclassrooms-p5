069b0f44e1cff9c630cc3d705091ece05a1773e9
Ludo <ludoloops@protonmail.com>
2020-06-07 18:53:03 +0200

    Refactor: cleanup tabs.js

--- p5-framework/src/modules/tabs.js
+++ p5-framework/src/modules/tabs.js
@@ -1,49 +1,6 @@
 export default (function () {
   console.log('hello');
 
-  const afficherOnglet = (a, animations = true) => {
-    const li = a.parentNode;
-    const div = a.parentNode.parentNode;
-    console.log(div);
-    div.classList.add('hidden');
-    document.getElementById('products_page').classList.remove('hidden');
-    const activeTab = div.querySelector('.tab-content.active'); // contenu actif
-    const target = div.querySelector(a.getAttribute('href')); // contenu à afficher
-    // console.log(activeTab);
-
-    if (li.classList.contains('active')) {
-      return false;
-    }
-    // on retire la classe active de l'onglet actif
-    div.querySelector('.tabs .active').classList.remove('active');
-    // ajout de la class active à l'onglet actuel
-    li.classList.add('active');
-
-    if (animations) {
-      activeTab.classList.add('fade');
-      activeTab.classList.remove('in');
-      const transitioned = function () {
-        this.classList.remove('fade');
-        this.classList.remove('active');
-        target.classList.add('active');
-        target.classList.add('fade');
-        target.classList.add('in');
-        activeTab.removeEventListener('transitioned', transitioned);
-      };
-
-      activeTab.addEventListener('transitioned', transitioned);
-    } else {
-      target.classList.add('active');
-      activeTab.classList.remove('active');
-    }
-  };
-  let tabs = document.querySelectorAll('.tabs a');
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', function () {
-      afficherOnglet(this);
-    });
-  }
-
   const hashChange = function (e) {
     const { hash } = window.location;
     console.log(hash);
@@ -51,17 +8,13 @@ export default (function () {
     if (hash === '#products_page') {
       console.log('hash', hash);
 
-      document.getElementById('products_page_hidden').classList.remove('hidden');
-      document
-        .getElementById('products_page')
-        .classList.add('transition', 'delay-150', 'duration-300', 'opacity-100');
       document.getElementById('hero').classList.add('hidden');
+      document.getElementById('menu_link_products').classList.add('border-purple-600');
+      document.getElementById('menu_link_home').classList.remove('border-purple-600');
     } else {
-      document.getElementById('products_page_hidden').classList.add('hidden');
       document.getElementById('hero').classList.remove('hidden');
-      document
-        .getElementById('products_page')
-        .classList.remove('transition', 'delay-150', 'duration-300', 'opacity-100');
+      document.getElementById('menu_link_products').classList.remove('border-purple-600');
+      document.getElementById('menu_link_home').classList.add('border-purple-600');
     }
   };
   window.addEventListener('hashchange', hashChange);
